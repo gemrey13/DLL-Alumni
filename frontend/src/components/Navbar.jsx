ae72e4b5746ae391394069c31f5a6017bfbe35d6
gemrey <gemreyranola@gmail.com>
2023-06-30 23:28:02 +0800

    Continue to add desin in sidebar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,15 +33,113 @@ const Navbar = () => {
 
     return (
         <div>
-        <nav  className={`navbar ${darkToggle ? 'dark:bg-gray-700 dark:bg-slate-900' : ''} transition-colors duration-500`}>
+        <nav  className='navbar dark:bg-gray-700 dark:bg-slate-900 bg-white transition-colors duration-500'>
             <div className="logo_item">
                 <i className="bx bx-menu" id="sidebarOpen"></i>
                 <img src="" alt="" /><span className='navbar-text dark:text-white'>DLL Alumni Office</span>
             </div>
-            <div className="navbar_content" onClick={darkSwitch}>
+            <div className="navbar_content dark:text-white" onClick={darkSwitch}>
                 <i className="bi bi-grid"></i>
                 <i className={`bx ${darkToggle? 'bx-moon' : 'bx-sun'}`} id="darkLight"></i>
                 <i className='bx bx-bell'></i>
+            </div>
+        </nav>
+
+        <nav className="sidebar close bg-white">
+            <div className="menu_content">
+                <ul className="menu_items">
+                    <div className="menu_title menu_dahsboard"></div>
+
+                    <li className="item">
+                        <a href="" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className="bx bx-grid-alt"></i>
+                            </span>
+                            <span className="navlink">Overview</span>
+                        </a>
+                    </li>
+
+                    <div href="#" className="nav_link submenu_item">
+                        <span className="navlink_icon">
+                            <i className="bx bx-home-alt"></i>
+                        </span>
+                        <span className="navlink">Home</span>
+                        <i className="bx bx-chevron-right arrow-left"></i>
+                    </div>
+                    <ul className="menu_items submenu">
+                        <a href="#" className="nav_link sublink">Under Development</a>
+                        <a href="#" className="nav_link sublink">Under Development</a>
+                    </ul>
+                    <li className="item">
+                        <a href="" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className='bx bx-user'></i>
+                            </span>
+                            <span className="navlink">Alumni</span>
+                        </a>
+                    </li>
+                    <li className="item">
+                        <a href="#" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className='bx bx-message-square-add'></i>
+                            </span>
+                            <span className="navlink">Post Request</span>
+                        </a>
+                    </li>
+                    <li className="item">
+                        <a href="#" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className='bx bx-book-bookmark'></i>
+                            </span>
+                            <span className="navlink">Tracer</span>
+                        </a>
+                    </li>
+ 
+                </ul>
+                <ul className="menu_items">
+                    <div className="menu_title menu_setting"></div>
+                    <li className="item">
+                        <a href="#" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className="bx bx-flag"></i>
+                            </span>
+                            <span className="navlink">Notification</span>
+                        </a>
+                    </li>
+                    <li className="item">
+                        <a href="#" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className="bx bx-medal"></i>
+                            </span>
+                            <span className="navlink">Award</span>
+                        </a>
+                    </li>
+                    <li className="item">
+                        <a href="#" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className="bx bx-cog"></i>
+                            </span>
+                            <span className="navlink">Setting</span>
+                        </a>
+                    </li>
+                    <li className="item">
+                        <a href="#" className="nav_link">
+                            <span className="navlink_icon">
+                                <i className="bx bx-layer"></i>
+                            </span>
+                            <span className="navlink">Features</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <div className="bottom_content">
+                    <div className="bottom expand_sidebar">
+                        <i className='bx bx-log-in'></i>
+                    </div>
+                    <div className="bottom collapse_sidebar">
+                        <i className='bx bx-log-out'></i>
+                    </div>
+                </div>
             </div>
         </nav>
         </div>
