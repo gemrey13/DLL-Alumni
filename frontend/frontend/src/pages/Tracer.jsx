6de977e63592872a8ab0ba2b93c35869f0ecad4d
gemreyranola <gemreyranola@gmail.com>
2023-06-30 04:41:57 +0000

    Added a frontend

---
+++ frontend/frontend/src/pages/Tracer.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from '../components/Navbar'
+
+
+function Tracer() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default Tracer
