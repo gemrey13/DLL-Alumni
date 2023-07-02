b7552657c58114633dadc75dde1ac1202a9e6fd3
gemrey <gemreyranola@gmail.com>
2023-07-02 09:19:59 +0800

    UI table course to status

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
 		<>
 			<div className='text-black dark:text-white ml-4 sm:mr-0 mt-20 sm:ml-20 sm:mt-24'>
         
-	        <div class="w-full border-t-2 px-8 py-6 lg:flex justify-between items-center bg-white">
-	              <p class="mb-2 lg:mb-0">© Copyright 2023</p>
+	        <div className="w-full border-t-2 px-8 py-6 lg:flex justify-between items-center bg-white">
+	              <p className="mb-2 lg:mb-0">© Copyright 2023</p>
 
-	              <div class="flex">
-	                  <a href="#" class="mr-6 hover:text-gray-900">Terms of Service</a>
-	                  <a href="#" class="mr-6 hover:text-gray-900">Privacy Policy</a>
-	                  <a href="#" class="hover:text-gray-900">About Us</a>
+	              <div className="flex">
+	                  <a href="#" className="mr-6 hover:text-gray-900">Terms of Service</a>
+	                  <a href="#" className="mr-6 hover:text-gray-900">Privacy Policy</a>
+	                  <a href="#" className="hover:text-gray-900">About Us</a>
 	              </div>
 	          </div>
 	      </div>
--- frontend/src/components/TableAlumni.jsx
+++ frontend/src/components/TableAlumni.jsx
@@ -10,7 +10,7 @@ const TableAlumni = () => {
               <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">ID</th>
               <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">First Name</th>
               <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">Last Name</th>
-              <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">Course</th>
+              <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">Staus</th>
               <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">Grad Year</th>
               <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">Email</th>
               <th className="w-auto px-3 py-4 font-semibold tracking-wide text-left">Mobile No.</th>
@@ -22,12 +22,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-200 rounded-full'>Employed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -35,12 +35,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-white bg-red-500 rounded-full'>Unemployed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -48,12 +48,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-200 rounded-full'>Employed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -61,12 +61,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-200 rounded-full'>Employed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -74,12 +74,24 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-white bg-red-500 rounded-full'>Unemployed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
+                <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
+              </td>
+            </tr><tr className="bg-white hover:-translate-y-1 transition-transform duration-200">
+              <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
+              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
+              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-white bg-red-500 rounded-full'>Unemployed</span></td>
+              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
+              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
+              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
+              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -87,12 +99,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-200 rounded-full'>Employed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -100,12 +112,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-white bg-red-500 rounded-full'>Unemployed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -113,12 +125,12 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-200 rounded-full'>Employed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
@@ -126,39 +138,28 @@ const TableAlumni = () => {
               <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+              <td className="w-auto whitespace-nowrap"><span className='inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-200 rounded-full'>Employed</span></td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
               <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
+                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">Edit</button>
                 <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
               </td>
             </tr>
-            <tr className="bg-white hover:-translate-y-1 transition-transform duration-200">
-              <td className="w-auto p-3 text-sm text-black whitespace-nowrap">A10000</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
-              <td className="w-auto p-3 text-sm text-gray-700 whitespace-nowrap text-center">
-                <button className="px-4 py-1 bg-blue-500 rounded-md text-white hover:bg-blue-700 hover:-translate-y-1 transition-transform duration-200">See</button>
-                <button className="ml-2 py-1 bg-orange-500 px-3 rounded-md text-white hover:bg-orange-700 hover:-translate-y-1 transition-transform duration-200">Delete</button>
-              </td>
-            </tr>
+
+            
             {/*{data.map((item, index) => (
                   <tr key={index} className={index === 0 ? 'bg-white' : index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">A10000</td>
-                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">BSIT</td>
+                    <td className="p-3 text-sm text-gray-700 whitespace-nowrap">Employed</td>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">Gem Rey</td>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">Ranola</td>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">2017</td>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">gemreyranola@gmail.com</td>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">09**********</td>
                     <td className="p-3 text-sm text-gray-700 whitespace-nowrap">
-                      <button>See</button>
+                      <button>Edit</button>
                       <button>Delete</button>
                     </td>
                   </tr>
@@ -174,12 +175,12 @@ const TableAlumni = () => {
         </div>
 
         <div className="text-gray-700 text-md font-semibold">
-          A10000 <span>BSIT</span>
+          A10000 <span>Employed</span>
         </div>
         <hr className="border-gray-400" />
         <div className="text-sm text-gray-700 flex justify-between">
           <span>gemreyranola@gmail.com</span>
-          <button className="px-4 py-1 bg-blue-500 rounded-md text-white">See</button>
+          <button className="px-4 py-1 bg-blue-500 rounded-md text-white">Edit</button>
         </div>
         <div className="text-sm text-gray-700 flex justify-between">
           <span>09**********</span>
