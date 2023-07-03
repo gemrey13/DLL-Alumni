90d23a80c7d8594dc2d69cd23e81b760b21a7882
gemrey <gemreyranola@gmail.com>
2023-07-03 11:07:37 +0800

    Some modification

--- frontend/src/hooks/useAxios.jsx
+++ frontend/src/hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
+ 
 const useAxios = (url) => {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
