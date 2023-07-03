5797785fe8c5eadcb099ea14641ca6602774b41d
gemrey <gemreyranola@gmail.com>
2023-07-03 10:57:12 +0800

    VIte config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  }
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000
+//   }
+// })
 
+export default {
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  base: '/tracer/',
+};
