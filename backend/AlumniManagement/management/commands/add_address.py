358a1a405fc932ed4fb774beece7029668b47f0a
gemreyranola <gemreyranola@gmail.com>
2023-07-06 06:00:41 +0000

    added address

--- backend/AlumniManagement/management/commands/add_address.py
+++ backend/AlumniManagement/management/commands/add_address.py
@@ -1,78 +1,36 @@
-import os
 import json
 from django.core.management.base import BaseCommand
 from AlumniManagement.models import Country, Region, Province, City, Barangay
 
 class Command(BaseCommand):
-    help = 'Import data from JSON file to database'
+    help = 'Imports data from a JSON file to the database.'
 
     def add_arguments(self, parser):
-        parser.add_argument('file_path', type=str, help='Path to the JSON file')
+        parser.add_argument('file_path', type=str, help='The path to the JSON file.')
 
     def handle(self, *args, **options):
         file_path = options['file_path']
+        with open(file_path) as file:
+            data = json.load(file)
+            self.import_data(data)
 
-        with open(file_path, 'r') as json_file:
-            data = json.load(json_file)
+    def import_data(self, data):
+        country = Country.objects.get(country_name='Philippines')
 
-            country = Country.objects.get(country_name='Philippines')
+        for region_code, region_data in data.items():
+            region_name = region_data['region_name']
+            region = Region.objects.create(region_name=region_name, country=country)
 
-            regions = []
-            provinces = []
-            cities = []
-            barangays = []
+            province_list = region_data['province_list']
+            for province_name, province_data in province_list.items():
+                province = Province.objects.create(province_name=province_name, region=region)
 
-            for region_code, region_data in data.items():
-                region_name = region_data['region_name']
-                
-                region = Region(region_name=region_name, country=country)
-                regions.append(region)
+                barangay_list = province_data['municipality_list']
+                for city_name, city_data in barangay_list.items():
+                    city = City.objects.create(city_name=city_name, province=province)
 
-            Region.objects.bulk_create(regions)  # Save regions before creating related objects
+                    barangay_list = city_data['barangay_list']
+                    barangays = [Barangay(barangay_name=barangay_name, city=city) for barangay_name in barangay_list]
+                    Barangay.objects.bulk_create(barangays)
 
-            for region_code, region_data in data.items():
-                region_name = region_data['region_name']
-                region = Region.objects.get(region_name=region_name)
-
-                province_list = region_data['province_list']
-                for province_name, province_data in province_list.items():
-                    province = Province(province_name=province_name, region=region)
-                    provinces.append(province)
-
-            Province.objects.bulk_create(provinces)  # Save provinces before creating related objects
-
-            for region_code, region_data in data.items():
-                region_name = region_data['region_name']
-                region = Region.objects.get(region_name=region_name)
-
-                province_list = region_data['province_list']
-                for province_name, province_data in province_list.items():
-                    province = Province.objects.get(province_name=province_name, region=region)
-
-                    municipality_list = province_data['municipality_list']
-                    for municipality_name, municipality_data in municipality_list.items():
-                        city = City(city_name=municipality_name, province=province)
-                        cities.append(city)
-
-            City.objects.bulk_create(cities)  # Save cities before creating related objects
-
-            for region_code, region_data in data.items():
-                region_name = region_data['region_name']
-                region = Region.objects.get(region_name=region_name)
-
-                province_list = region_data['province_list']
-                for province_name, province_data in province_list.items():
-                    province = Province.objects.get(province_name=province_name, region=region)
-
-                    municipality_list = province_data['municipality_list']
-                    for municipality_name, municipality_data in municipality_list.items():
-                        city = City.objects.get(city_name=municipality_name, province=province)
-
-                        barangay_list = municipality_data['barangay_list']
-                        for barangay_name in barangay_list:
-                            barangay = Barangay(barangay_name=barangay_name, city=city)
-                            barangays.append(barangay)
-
-            Barangay.objects.bulk_create(barangays)
-
-        self.stdout.write(self.style.SUCCESS('Data imported successfully!'))
+        self.stdout.write(self.style.SUCCESS('Address imported successfully!'))
