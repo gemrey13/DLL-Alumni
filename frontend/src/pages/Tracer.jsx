d9dc89cdcaf3a843fc127c6acd5ee05d248710ed
gemreyranola <gemreyranola@gmail.com>
2023-07-07 04:39:45 +0000

    MOdified Tracer page

--- frontend/src/pages/Tracer.jsx
+++ frontend/src/pages/Tracer.jsx
@@ -34,10 +34,11 @@ const Tracer = () => {
           </div>
         </div>
 
-        {isModal && (
-          <div className="relative z-10 transition-opacity" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
+        <div className=''></div>
 
+        {isModal && (
+          <div className="relative z-10 transition-all duration-1000 ease-in-out inset-0 bg-gray-500 bg-opacity-75" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
             <div className="fixed inset-0 z-10 overflow-y-auto">
               <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                 <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
@@ -54,17 +55,18 @@ const Tracer = () => {
                       </div>
                       <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                         <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">
-                          Deactivate account
+                          Create Alumni
                         </h3>
                         <div className="mt-2">
+                          <AlumniForm />
                           <p className="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
                         </div>
                       </div>
                     </div>
                   </div>
                   <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button onClick={() => setIsModal(false)} type="button" className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">
-                      Deactivate
+                    <button onClick={() => setIsModal(false)} type="button" className="inline-flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 sm:ml-3 sm:w-auto">
+                      Submit
                     </button>
                     <button
                       onClick={() => setIsModal(false)}
@@ -82,7 +84,6 @@ const Tracer = () => {
 
         <TracerAlumniTable />
 
-        <AlumniForm />
       </div>
 
       <Footer />
