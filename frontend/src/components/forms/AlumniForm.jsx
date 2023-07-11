51c05faf427eeb95fe8212a6b56b175159d77e9e
gemreyranola <gemreyranola@gmail.com>
2023-07-11 02:15:10 +0000

    Working in Alumni Form'

--- frontend/src/components/forms/AlumniForm.jsx
+++ frontend/src/components/forms/AlumniForm.jsx
@@ -11,6 +11,7 @@ const AlumniForm = () => {
   const [provinces, setProvinces] = useState([]);
   const [cities, setCities] = useState([]);
   const [barangays, setBarangays] = useState([]);
+  
 
   const { data: countriesData, isLoading: countriesLoading, error: countriesError } = useAxios('api/address/countries/');
   const { data: regionsData, isLoading: regionsLoading, error: regionsError } = useAxios(`api/address/countries/${selectedCountry}/regions/`);
@@ -85,8 +86,33 @@ const AlumniForm = () => {
   };
 
   return (
-    <div className='mt-10'>
-      <select onChange={handleCountryChange}>
+    <>
+
+<div className="relative z-10 transition-all duration-1000 ease-in-out inset-0 bg-gray-500 bg-opacity-75" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75"></div>
+            <div className="fixed inset-0 z-10 overflow-y-auto">
+              <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all ">
+
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                        <svg className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                          />
+                        </svg>
+                      </div>
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">
+                          Create Alumni
+                        </h3>
+                        <div className="mt-2">
+                          
+    <div className='flex justify-center items-center'>
+      <select onChange={handleCountryChange} className=' dark:bg-gray-700 dark:text-white'>
         <option value="">Select Country</option>
         {countries.map((country) => (
           <option key={country.id} value={country.id}>
@@ -95,7 +121,7 @@ const AlumniForm = () => {
         ))}
       </select>
 
-      <select onChange={handleRegionChange}>
+      <select onChange={handleRegionChange} className='dark:bg-gray-700 dark:text-white'>
         <option value="">Select Region</option>
         {regions.map((region) => (
           <option key={region.id} value={region.id}>
@@ -105,7 +131,7 @@ const AlumniForm = () => {
       </select>
 
 
-      <select onChange={handleProvinceChange}>
+      <select onChange={handleProvinceChange} className='dark:bg-gray-700 dark:text-white'>
         <option value="">Select Province</option>
         {provinces.map((province) => (
           <option key={province.id} value={province.id}>
@@ -114,7 +140,7 @@ const AlumniForm = () => {
         ))}
       </select>
 
-      <select onChange={handleCityChange}>
+      <select onChange={handleCityChange} className='dark:bg-gray-700 dark:text-white'>
         <option value="">Select City</option>
         {cities.map((city) => (
           <option key={city.id} value={city.id}>
@@ -123,7 +149,7 @@ const AlumniForm = () => {
         ))}
       </select>
 
-      <select>
+      <select className='dark:bg-gray-700 dark:text-white'>
         <option value="">Select Barangay</option>
         {barangays.map((barangay) => (
           <option key={barangay.id} value={barangay.id}>
@@ -132,6 +158,30 @@ const AlumniForm = () => {
         ))}
       </select>
     </div>
+                          <p className="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button onClick={() => setIsModal(false)} type="button" className="inline-flex w-full justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 sm:ml-3 sm:w-auto">
+                      Submit
+                    </button>
+                    <button
+                      onClick={() => setIsModal(false)}
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    
+    </>
   );
 };
 
