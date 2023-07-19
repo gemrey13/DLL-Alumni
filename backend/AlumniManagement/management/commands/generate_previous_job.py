3e735b8587453318b2cd1941506bc4dff280d638
gemrey <gemreyranola@gmail.com>
2023-07-19 10:21:31 +0800

    Added a prev job command

--- backend/AlumniManagement/management/commands/generate_previous_job.py
+++ backend/AlumniManagement/management/commands/generate_previous_job.py
@@ -34,9 +34,11 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('count', type=int, help='Number of records to generate')
+        parser.add_argument('--no-prev-job-prob', type=float, default=0.2, help='Probability of alumni without previous jobs')
 
     def handle(self, *args, **options):
         count = options['count']
+        no_prev_job_prob = options['no_prev_job_prob']
         alumni_profiles = AlumniProfile.objects.all()
         fake = Faker()
 
@@ -47,60 +49,75 @@ class Command(BaseCommand):
         barangays = JobAddress.objects.values_list('barangay', flat=True).distinct()
 
         for alumni in alumni_profiles:
-            num_previous_jobs = random.randint(1, count)
+            generate_prev_job = random.random() > no_prev_job_prob
 
-            for _ in range(num_previous_jobs):
-                current_job = alumni.currentjob_set.last()  # Get the most recent current job
+            if generate_prev_job:
+                num_previous_jobs = random.randint(1, count)
 
-                if current_job:
-                    start_date = current_job.start_date
-                else:
-                    start_date = alumni.graduate_set.last().graduation_date
+                for _ in range(num_previous_jobs):
+                    current_job = alumni.currentjob_set.last()  # Get the most recent current job
 
-                max_days_after_start = (date.today() - start_date).days
+                    if current_job:
+                        start_date = current_job.start_date
+                    else:
+                        start_date = alumni.graduate_set.last().graduation_date
 
-                if max_days_after_start < 0:
-                    max_days_after_start = 0  # Set to 0 if negative
+                    max_days_after_start = (date.today() - start_date).days
 
-                end_date = start_date + timedelta(days=random.randint(0, max_days_after_start))
+                    if max_days_after_start < 0:
+                        max_days_after_start = 0  # Set to 0 if negative
 
-                if end_date < start_date:
-                    start_date, end_date = end_date, start_date
+                    end_date = start_date + timedelta(days=random.randint(0, max_days_after_start))
 
-                country = random.choice(countries)
-                region = random.choice(regions.filter(country=country))
-                province = random.choice(provinces.filter(region=region))
-                city = random.choice(cities.filter(province=province))
-                barangay = random.choice(barangays.filter(city=city))
-                street = fake.street_address()
+                    if end_date < start_date:
+                        start_date, end_date = end_date, start_date
 
-                job_address = JobAddress.objects.create(
-                    country_id=country,
-                    region_id=region,
-                    province_id=province,
-                    city_id=city,
-                    barangay_id=barangay,
-                    street=street
-                )
+                    country = random.choice(countries)
+                    region = random.choice(regions.filter(country=country))
+                    province = random.choice(provinces.filter(region=region))
+                    city = random.choice(cities.filter(province=province))
+                    barangay = random.choice(barangays.filter(city=city))
+                    street = fake.street_address()
 
-                previous_job_id = fake.random_int(min=100000, max=999999)
-                job_type = random.choice([choice[0] for choice in FIELD_CHOICES])
-                job_title = fake.job()
-                salary = random.randint(1000, 10000)
-                company_name = fake.company()
+                    job_address = JobAddress.objects.create(
+                        country_id=country,
+                        region_id=region,
+                        province_id=province,
+                        city_id=city,
+                        barangay_id=barangay,
+                        street=street
+                    )
 
-                previous_job = PreviousJob.objects.create(
-                    previous_job_id=previous_job_id,
-                    job_type=job_type,
-                    job_title=job_title,
-                    salary=salary,
-                    start_date=start_date,
-                    end_date=end_date,
-                    company_name=company_name,
-                    alumni=alumni,
-                    address=job_address
-                )
+                    unique_previous_job_id = self.generate_unique_previous_job_id()
+                    job_type = random.choice([choice[0] for choice in FIELD_CHOICES])
+                    job_title = fake.job()
+                    salary = random.randint(1000, 100000)
+                    company_name = fake.company()
 
-                self.stdout.write(self.style.SUCCESS(f'Successfully created PreviousJob: {previous_job}'))
+                    previous_job = PreviousJob.objects.create(
+                        previous_job_id=unique_previous_job_id,
+                        job_type=job_type,
+                        job_title=job_title,
+                        salary=salary,
+                        start_date=start_date,
+                        end_date=end_date,
+                        company_name=company_name,
+                        alumni=alumni,
+                        address=job_address
+                    )
+
+                    self.stdout.write(self.style.SUCCESS(f'Successfully created PreviousJob: {previous_job}'))
 
             self.stdout.write(self.style.SUCCESS(f'Successfully processed AlumniProfile: {alumni}'))
+
+    def generate_unique_previous_job_id(self):
+        existing_ids = set(PreviousJob.objects.values_list('previous_job_id', flat=True))
+        unique_id = None
+
+        while not unique_id:
+            generated_id = random.randint(100000, 999999)
+            if generated_id not in existing_ids:
+                unique_id = generated_id
+
+        return unique_id
+
