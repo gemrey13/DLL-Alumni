af6cb0eff0fb1bd9c438330fc9df73790179a2ee
gemrey <gemreyranola@gmail.com>
2023-07-20 09:14:46 +0800

    changed button to span

--- frontend/src/pages/Tracer.jsx
+++ frontend/src/pages/Tracer.jsx
@@ -66,7 +66,7 @@ const Tracer = () => {
               </select>
             </span>
           </form>
-          
+
           <form className="flex">
             <span className="flex">
               <input type="text" className="sm:focus:w-72 w-52 p-2 rounded-md bg-gray-200 text-gray-600 dark:bg-slate-700 dark:text-white transition-width duration-500" placeholder="Search Alumni . . . ." />
@@ -75,15 +75,15 @@ const Tracer = () => {
               </button>
             </span>
 
-            <span onClick={() => setIsModal(true)} className="ml-2 mr-4 sm:ml-3 sm:mr-2 transition-transform duration-200 hover:scale-110">
-              <button>
+            <span onClick={() => setIsModal(true)} className="ml-2 mr-4 sm:ml-3 sm:mr-2 transition-transform duration-200 hover:scale-110 cursor-pointer">
+              <span>
                 <i className="bx bxs-plus-square text-blue-600 hover:text-blue-800 text-4xl"></i>
-              </button>
+              </span>
             </span>
             <span className="mr-2 sm:mr-6 transition-transform duration-200 hover:scale-110">
-              <button>
+              <span>
                 <i className="bx bx-filter text-black dark:text-white hover:font-black text-4xl"></i>
-              </button>
+              </span>
             </span>
           </form>
         </div>
