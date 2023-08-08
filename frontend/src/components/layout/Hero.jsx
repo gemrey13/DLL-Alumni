d7b2bf445a25026f22bad7bae244fb386b879a80
gemrey <gemreyranola@gmail.com>
2023-08-08 12:00:00 +0800

    Hero section in default pages

--- frontend/src/components/layout/Hero.jsx
+++ frontend/src/components/layout/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
         <div>
           <h1>Welcome to Alumni Web Portal - Empowering Education, Connecting Alumni!</h1>
           <p>Your All-in-One Online Portal and Alumni Office Information System with Tracer Study for Curriculum Analysis</p>
+          <button>Get Started</button>
         </div>
         <div>
           <h1>
