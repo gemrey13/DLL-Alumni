0998a5b52b9b0982b207f519319f36d6d4ee14fa
gemrey <gemreyranola@gmail.com>
2023-08-11 12:00:00 +0800

    News And Update section in default pages

--- frontend/src/components/layout/NewsUpdate.jsx
+++ frontend/src/components/layout/NewsUpdate.jsx
@@ -4,7 +4,16 @@ const NewsUpdate = () => {
   return (
     <>
       <div>
-      NewsUpdate
+        <div>Slide</div>
+        <div>
+          <h3>Celebrating a Milestone - 100,000 Strong Alumni Network!</h3>
+          <h6>August 21, 2023</h6>
+          <p>
+            We are delighted to announce that DAOIS has reached a significant milestone - our alumni 
+            network has surpassed 100,000 members! We extend our heartfelt gratitude to all our alumni
+            for being an integral part of this thriving community.
+          </p>
+        </div>
       </div>
     </>
   );
