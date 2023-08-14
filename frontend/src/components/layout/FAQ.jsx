2482208302c35b040aa4d41da889a0fadbb1813e
gemrey <gemreyranola@gmail.com>
2023-08-14 12:00:00 +0800

    FAQ section in default pages

--- frontend/src/components/layout/FAQ.jsx
+++ frontend/src/components/layout/FAQ.jsx
@@ -4,7 +4,13 @@ const FAQ = () => {
   return (
     <>
       <div>
-      FAQ
+      <h1>Frequently Ask Questions (FAQ)</h1>
+      <ul>
+        <li>What is DAOIS, and how does it work?</li>
+        <li>How can I sign up for DAOIS?</li>
+        <li>Is DAOIS only for alumni, or can current students also use it?</li>
+        <li>What is the Tracer Study Feature, and how does it help curriculum analysis?</li>
+      </ul>
       </div>
     </>
   );
