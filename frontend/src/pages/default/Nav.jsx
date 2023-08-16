7e43f7926a4f6b2fe02ef2d00eef24a9054811ca
gemrey <gemreyranola@gmail.com>
2023-08-16 12:00:00 +0800

    design default nav

--- frontend/src/pages/default/Nav.jsx
+++ frontend/src/pages/default/Nav.jsx
@@ -4,12 +4,14 @@ import React from 'react';
 const Nav = () => {
 	return (
 		<>
-			<nav>
-				<span>Logo</span>
-				<span>Home</span>
-				<span>About</span>
-				<span>News & Update</span>
-				<span>Sign In</span>
+			<nav className='flex justify-between items-center'>
+				<div className='flex justify-between items-center justify-between w-[50%]'>
+					<h5 className='text-5xl'>Logo</h5>
+					<h5>Home</h5>
+					<h5>About</h5>
+					<h5>News & Update</h5>
+				</div>
+				<h5 className='w-1/5'>Sign In</h5>
 			</nav>
 		</>
 	)
