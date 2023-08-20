e68fbbea37521bb7f6a65f133edf7da1f67d5dd9
gemrey <gemreyranola@gmail.com>
2023-08-20 12:00:00 +0800

    design default hero section

--- frontend/src/components/layout/Hero.jsx
+++ frontend/src/components/layout/Hero.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Hero = () => {
   return (
     <>
-      <div>
+      <div className='flex px-[4em] py-[5em] justify-between'>
         <div>
-          <h1>Welcome to Alumni Web Portal - Empowering Education, Connecting Alumni!</h1>
-          <p>Your All-in-One Online Portal and Alumni Office Information System with Tracer Study for Curriculum Analysis</p>
+          <h1 className='text-2xl text-black font-bold'>Welcome to Alumni Web Portal - <br/>Empowering Education, Connecting Alumni!</h1>
+          <p>Your All-in-One Online Portal and Alumni Office Information <br/>System with Tracer Study for Curriculum Analysis</p>
           <button>Get Started</button>
         </div>
         <div>
