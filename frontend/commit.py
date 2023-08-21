39c6add381b6b55c344cf6d6754d14d7354053ee
gemrey <gemreyranola@gmail.com>
2023-08-21 09:43:25 +0800

    disgn hero sesction

---
+++ frontend/commit.py
@@ -0,0 +1,13 @@
+import subprocess
+
+# Prompt the user for a commit message
+commit_message = input("Enter your commit message: ")
+
+# Run git commands
+try:
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", commit_message], check=True)
+    subprocess.run(["git", "push", "origin", "main"], check=True)
+    print("Git commands executed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error executing Git command: {e}")
