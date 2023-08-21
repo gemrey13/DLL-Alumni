890a7f318f841c0672648a17198224bc1a479d40
gemrey <gemreyranola@gmail.com>
2023-08-21 09:36:51 +0800

    Design hero section

--- frontend/src/components/layout/Hero.jsx
+++ frontend/src/components/layout/Hero.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Hero = () => {
   return (
     <>
-      <div className='flex px-[4em] py-[5em] justify-between'>
+      <div className='flex px-[4em] py-[4em] justify-between items-center'>
         <div>
-          <h1 className='text-3xl text-black font-bold'>Welcome to Alumni Web Portal - <br/>Empowering Education, Connecting Alumni!</h1>
-          <p>Your All-in-One Online Portal and Alumni Office Information <br/>System with Tracer Study for Curriculum Analysis</p>
-          <button>Get Started</button>
+          <h1 className='text-3xl text-black font-bold'>Welcome to Alumni Web Portal - <br/>Empowering Education, Connecting<br/> Alumni!</h1>
+          <p className='pt-4'>Your All-in-One Online Portal and Alumni Office Information <br/>System with Tracer Study for Curriculum Analysis</p>
+          <button className=''>Get Started</button>
         </div>
         <div>
-          <h1>
-            Picture
-          </h1>
+          <div className='w-[500px] h-[400px] bg-green-700'>
+            <div className='w-[500px] h-[400px] bg-green-600 relative -left-10 -bottom-10'></div>
+          </div>
         </div>
       </div>
     </>
--- frontend/src/pages/default/Nav.jsx
+++ frontend/src/pages/default/Nav.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Nav = () => {
 	return (
 		<>
-			<nav className='flex justify-between items-center px-[4em] py-[1em]'>
+			<nav className='flex justify-between items-center px-[4em] py-[2em]'>
 				<div className='flex justify-between items-center justify-between w-[50%] text-black-1'>
 					<h5 className='text-5xl'>Logo</h5>
 					<h5>Home</h5>
