acfb21973c7c427fc15d1bf3e9b23903aa8bb7fd
gemrey <gemreyranola@gmail.com>
2023-08-21 08:33:42 +0800

    Design hero section

--- frontend/src/components/layout/Hero.jsx
+++ frontend/src/components/layout/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <>
       <div className='flex px-[4em] py-[5em] justify-between'>
         <div>
-          <h1 className='text-2xl text-black font-bold'>Welcome to Alumni Web Portal - <br/>Empowering Education, Connecting Alumni!</h1>
+          <h1 className='text-3xl text-black font-bold'>Welcome to Alumni Web Portal - <br/>Empowering Education, Connecting Alumni!</h1>
           <p>Your All-in-One Online Portal and Alumni Office Information <br/>System with Tracer Study for Curriculum Analysis</p>
           <button>Get Started</button>
         </div>
