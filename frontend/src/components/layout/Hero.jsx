8a9b4d5c9f40912a8dbac4124975084181d52360
gemrey <gemreyranola@gmail.com>
2023-08-21 09:49:14 +0800

    design hero section

--- frontend/src/components/layout/Hero.jsx
+++ frontend/src/components/layout/Hero.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Hero = () => {
   return (
     <>
-      <div className='flex px-[4em] py-[4em] justify-between items-center'>
-        <div>
+      <div className='flex px-[4em] py-[4em] items-center'>
+        <div className='w-1/2'>
           <h1 className='text-3xl text-black font-bold'>Welcome to Alumni Web Portal - <br/>Empowering Education, Connecting<br/> Alumni!</h1>
           <p className='pt-4'>Your All-in-One Online Portal and Alumni Office Information <br/>System with Tracer Study for Curriculum Analysis</p>
-          <button className=''>Get Started</button>
+          <button className='relative bottom-0 left-[50%] mt-[2em]'>Get Started</button>
         </div>
-        <div>
+        <div className='w-1/2 flex justify-center'>
           <div className='w-[600px] h-[400px] bg-green-700'>
             <div className='w-[600px] h-[400px] bg-green-600 relative -left-12 -bottom-12'></div>
           </div>
