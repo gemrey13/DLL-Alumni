8ad919d721137e57721fe3251b84a5fb842bec14
gemrey13 <gemreyranola@gmail.com>
2023-10-28 13:41:31 +0800

    added some style in header

--- frontend/src/pages/default_side/component/Header.jsx
+++ frontend/src/pages/default_side/component/Header.jsx
@@ -2,26 +2,17 @@ import React from 'react'
 
 export default function Nav() {
   return (
-	// <nav className='flex justify-between items-center px-[4em] py-[2em]'>
-	// 	<div className='flex justify-between items-center justify-between w-[50%] text-black-1'>
-	// 		<h5 className='text-5xl'>Logo</h5>
-	// 		<h5>Home</h5>
-	// 		<h5>About</h5>
-	// 		<h5>News & Update</h5>
-	// 	</div>
-	// 	<h5 className='w-1/5 text-black text-lg text-right'>Sign In</h5>
-	// </nav>
-	<div class="navbar bg-green-800">
-		<div class="navbar-start">
-			<div class="dropdown">
-			<label tabindex="0" class="btn btn-ghost lg:hidden">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+	<div className="navbar bg-white">
+		<div className="navbar-start">
+			<div className="dropdown">
+			<label tabIndex="0" className="btn btn-ghost lg:hidden">
+				<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 			</label>
-			<ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+			<ul tabIndex="0" className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-slate-700 rounded-box w-52">
 				<li><a>Home</a></li>
 				<li>
 				<a>News & Update</a>
-				<ul class="p-2">
+				<ul className="p-2">
 					<li><a>Submenu 1</a></li>
 					<li><a>Submenu 2</a></li>
 				</ul>
@@ -29,16 +20,16 @@ export default function Nav() {
 				<li><a>About</a></li>
 			</ul>
 			</div>
-			<a class="btn btn-ghost normal-case text-xl text-white">DLL Alumni</a>
+			<a className="btn btn-ghost normal-case md:text-2xl text-xl text-slate-900 font-bold">DLL Alumni</a>
 		</div>
 
-		<div class="navbar-center hidden lg:flex font-mono ">
-			<ul class="menu menu-horizontal px-1 text-white text-lg">
+		<div className="navbar-center hidden lg:flex font-mono">
+			<ul className="menu menu-horizontal px-1 text-slate-700 text-lg">
 			<li><a>Home</a></li>
-			<li tabindex="0">
+			<li tabIndex="0">
 				<details>
 				<summary>News & Update</summary>
-				<ul class="p-2 text-black">
+				<ul className="p-2 text-black">
 					<li><a>Submenu 1</a></li>
 					<li><a>Submenu 2</a></li>
 				</ul>
@@ -47,8 +38,8 @@ export default function Nav() {
 			<li><a>About</a></li>
 			</ul>
 		</div>
-		<div class="navbar-end">
-			<a class="btn">Sign In</a>
+		<div className="navbar-end">
+			<a className="btn bg-sky-400">Sign In</a>
 		</div>
 	</div>
   )
--- frontend/src/pages/default_side/landing/Hero.jsx
+++ frontend/src/pages/default_side/landing/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
   return (
     <div className="hero min-h-screen bg-green-800">
         <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={sample} className="max-w-sm rounded-lg shadow-2xl" />
+            <img src={sample} className="max-w-sm rounded-lg shadow-2xl w-72 lg:w-96" />
             <div>
             <h1 className="text-5xl font-bold">Box Office News!</h1>
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
