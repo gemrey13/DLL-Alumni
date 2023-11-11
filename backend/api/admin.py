b2cfb3aec97f702528301c010eef6943a520105c
gemrey13 <gemreyranola@gmail.com>
2023-11-12 06:40:41 +0800

    register to admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,11 +1,37 @@
 from django.contrib import admin
-from .models import *
+from .models import (
+    AlumniProfile,
+    GraduateInformation,
+    Curriculum,
+    Course,
+    Department,
+    CurrentJob,
+    PreviousJob,
+    Address,
+    Country,
+    Region,
+    Province,
+    City,
+    Barangay
+)
 
-# Register your models here.
-admin.site.register(AlumniProfile)
+class AlumniProfileAdmin(admin.ModelAdmin):
+    list_display = ['alumni_id', 'fname', 'lname', 'course', 'date_of_birth']
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['country', 'region', 'province', 'city', 'barangay']
+
+
+admin.site.register(AlumniProfile, AlumniProfileAdmin)
 admin.site.register(GraduateInformation)
 admin.site.register(Curriculum)
 admin.site.register(Course)
 admin.site.register(Department)
 admin.site.register(CurrentJob)
 admin.site.register(PreviousJob)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(Province)
+admin.site.register(City)
+admin.site.register(Barangay)
