81e5a19a7a7221dbfa77e5b59638cab121861629
gemrey13 <gemreyranola@gmail.com>
2023-11-12 06:08:14 +0800

    added basic models

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class AlumniProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     alumni_id = models.CharField(primary_key=True, max_length=6)
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
     fname = models.CharField(max_length=64)
     lname = models.CharField(max_length=64)
     mi = models.CharField(max_length=2, blank=True, null=True)
@@ -13,7 +14,7 @@ class AlumniProfile(models.Model):
     religion = models.CharField(max_length=64)
     marital_status = models.CharField(max_length=64)
     date_of_birth = models.DateField()
-    course = models.CharField(max_length=6)
+    address = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f'ID: {self.alumni_id} - {self.fname} {self.lname}'
@@ -28,7 +29,7 @@ class GraduateInformation(models.Model):
 class Curriculum(models.Model):
     cmo_no = models.CharField(primary_key=True, max_length=10)
     description = models.CharField(max_length=64)
-    curriculum_year = models.DateField()
+    curriculum_year = models.IntegerField()
 
 
 class Course(models.Model):
@@ -54,21 +55,21 @@ class Department(models.Model):
 
 
 class CurrentJob(models.Model):
-    alumni = models.OneToOneField(AlumniProfile, on_delete=models.CASCADE, primary_key=True)
-    job_type = models.CharField(max_length=64, default='technology')
+    alumni = models.ForeignKey(AlumniProfile, on_delete=models.CASCADE)
+    job_type = models.CharField(max_length=64)
     job_title = models.CharField(max_length=64)
     salary = models.IntegerField()
     start_date = models.DateField()
     company_name = models.CharField(max_length=64)
-    address = models.IntegerField()
+    address = models.CharField(max_length=255, blank=True)
 
 
 class PreviousJob(models.Model):
-    alumni = models.OneToOneField(AlumniProfile, on_delete=models.CASCADE, primary_key=True)
-    job_type = models.CharField(max_length=64, default='technology')
+    alumni = models.ForeignKey(AlumniProfile, on_delete=models.CASCADE)
+    job_type = models.CharField(max_length=64)
     job_title = models.CharField(max_length=64)
     salary = models.IntegerField()
     start_date = models.DateField()
     end_date = models.DateField()
     company_name = models.CharField(max_length=64)
-    address = models.IntegerField()
+    address = models.CharField(max_length=255, blank=True)
