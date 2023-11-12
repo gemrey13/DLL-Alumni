b820029617e6eaedcda1c7560a01176999e2cb72
gemrey13 <gemreyranola@gmail.com>
2023-11-12 13:35:11 +0800

    Re-Initialize the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Tracer, Sample, Dashboard, Analysis, Home, AlumniProfile, Landing } from './index';
+import './App.css'
 
-const App = () => {
+function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Landing />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/news" element={<News />} /> */}
-        </Route>
-        
-        <Route path="/tracer" element={<Tracer />} />
-        <Route path="/tracer/profile/:alumniId" element={<AlumniProfile />} />
-        <Route path="/analysis" element={<Analysis />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/sample" element={<Sample />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    <>
+    </>
+  )
+}
 
-export default App;
+export default App
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-    },
-  },
 })
-
-// export default {
-//   server: {
-//     host: 'localhost',
-//     port: 3000,
-//   },
-//   base: '/',
-// };
