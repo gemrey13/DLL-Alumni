a83dce04101dfa944717a5df852c5cb4155bd662
gemrey13 <gemreyranola@gmail.com>
2023-12-04 07:51:42 +0800

    Added  design in mobile view

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -3,9 +3,9 @@ import sir_rovie from '../../images/sir_rovie.png'
 
 function Hero() {
   return (
-    <div className="hero mt-32">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className='relative'>
+    <div className="hero mt-12 lg:mt-32 px-5">
+        <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+            <div className='relative hidden md:block'>
                 <img src={sir_rovie} className="max-w-sm rounded-lg shadow-2xl w-72" />
                 <div className="w-96 h-48 bg-yellow-400 absolute z-[-100] -bottom-0 -left-36" />
                 <div className="w-80 h-96 bg-yellow-400 absolute z-[-100] top-36 left-16" />
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ import Hero from '../components/landing/Hero'
 function LandingPage() {
   return (
     <>
-        <div className="w-full h-[820px] md:px-16  sm:px-0 z-[-2] bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950">
+        <div className="w-full h-[720px] md:h-[820px] md:px-16  sm:px-0 z-[-2] bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950">
             <Navbar />
             <img src={blob_hero} alt='Blob 1' className='absolute -left-72'/>
             
