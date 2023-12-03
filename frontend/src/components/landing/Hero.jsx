98cca7a650639a5973c817f88d959b3f3adb2b06
gemrey13 <gemreyranola@gmail.com>
2023-12-04 07:37:54 +0800

    Added Hero section for desktop view

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -3,13 +3,18 @@ import sir_rovie from '../../images/sir_rovie.png'
 
 function Hero() {
   return (
-    <div className="hero min-h-screen">
+    <div className="hero mt-32">
         <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src={sir_rovie} className="max-w-sm rounded-lg shadow-2xl" />
+            <div className='relative'>
+                <img src={sir_rovie} className="max-w-sm rounded-lg shadow-2xl w-72" />
+                <div className="w-96 h-48 bg-yellow-400 absolute z-[-100] -bottom-0 -left-36" />
+                <div className="w-80 h-96 bg-yellow-400 absolute z-[-100] top-36 left-16" />
+            </div>
             <div>
-                <h1 className="text-yellow-500 text-5xl font-bold leading-10">Welcome to Alumni Web Portal!</h1>
-                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <button className="btn btn-primary">Get Started</button>
+                <h1 className="text-yellow-500 text-5xl font-bold leading-10">Welcome to Alumni Web Portal</h1>
+                <h1 className="text-white text-5xl font-bold leading-10">Empowering Education, <br/>Connecting Alumni!</h1>
+                <p className="py-6 text-gray-400">Your All-in-One Online Portal and Alumni Office Information <br/>System with Tracer Study for Curriculum Analysis.</p>
+                <button className="btn btn-warning text-white">Get Started</button>
             </div>
         </div>
     </div>
