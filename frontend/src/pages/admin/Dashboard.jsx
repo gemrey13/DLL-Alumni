04a69d70cb9f05c0697f0f5b91b246eac60b719d
gemrey13 <gemreyranola@gmail.com>
2023-12-05 21:19:57 +0800

    Some minor changes

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -9,17 +9,16 @@ function Dashboard() {
             <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            {/* Page content here */}dsfds
+            {/* Page content here */}dsfds 
 
         </div> 
         <div className="drawer-side">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-            <ul className="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
-            {/* Sidebar content here */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
+            <ul className="menu p-4 w-64 min-h-full bg-base-200 border-r border-r-gray-300 text-base-content">
+                {/* Sidebar content here */}
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
             </ul>
-        
         </div>
     </div>
     </>
