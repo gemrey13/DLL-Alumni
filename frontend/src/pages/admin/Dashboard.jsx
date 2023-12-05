3371da281f39ec731e9c108f22baf2cb7d7e4e04
gemrey13 <gemreyranola@gmail.com>
2023-12-05 21:06:24 +0800

    Added basic sidebar

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -14,7 +14,7 @@ function Dashboard() {
         </div> 
         <div className="drawer-side">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <ul className="menu p-4 w-72 min-h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
             <li><a>Sidebar Item 1</a></li>
             <li><a>Sidebar Item 2</a></li>
