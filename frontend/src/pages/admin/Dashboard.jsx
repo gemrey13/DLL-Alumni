3f5d74cc8a5d3ca641aa826304af2285e66b36d0
gemrey13 <gemreyranola@gmail.com>
2023-12-05 22:04:53 +0800

    Added some design in dashboard sidebar

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import icon_alumni from '../../images/icon-alumni.png'
+
 
 function Dashboard() {
   return (
@@ -14,10 +16,35 @@ function Dashboard() {
         </div> 
         <div className="drawer-side">
             <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label> 
-            <ul className="menu p-4 w-64 min-h-full bg-base-200 border-r border-r-gray-300 text-base-content">
+            <ul className="menu flex justify-between p-4 w-64 min-h-full bg-base-200 border-r border-r-gray-300 text-base-content">
+
+                <div className='flex justify-center items-center'>
+                    <div className='w-[40%] flex justify-center'>
+                        <img src={icon_alumni} alt="icon_alumni" />
+                    </div>
+                    <h1 className='text-xl text-purple-950 font-semibold'>DLL Alumni Association</h1>
+                </div>
                 {/* Sidebar content here */}
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <div className='text-base'>
+                    <li><a>Dashboard</a></li>
+                    <li><a>Trace Alumni</a></li>
+                    <li><a>Visualization</a></li>
+                    <li><a>Import Data</a></li>
+                    <li><a>Settings</a></li>
+                </div>
+
+                <div className='flex justify-evenly'>
+                    <div className="avatar">
+                        <div className="w-12 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <h1 className='text-xl text-purple-950 font-semibold'>Gem Rey Rañola</h1>
+                        <p className='font-medium text-gray-400'>Admin</p>
+                    </div>
+                </div>
             </ul>
         </div>
     </div>
