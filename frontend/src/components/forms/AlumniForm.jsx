1201e1ddefd1ce1d2e20cbaa8879ee6980e5b703
gemrey13 <gemreyranola@gmail.com>
2023-12-28 19:30:50 +0800

    ci: minor change into the employment record

--- frontend/src/components/forms/AlumniForm.jsx
+++ frontend/src/components/forms/AlumniForm.jsx
@@ -681,18 +681,38 @@ const AlumniForm = () => {
                           />
                         </td>
                         <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                          <input
-                            type="text"
-                            value={row.employmentStatus}
-                            onChange={(e) =>
-                              handleInputChange(
-                                index,
-                                "employmentStatus",
-                                e.target.value
-                              )
-                            }
-                            className="w-full rounded border-[1.5px] border-stroke bg-transparent py-2 px-3 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-                          />
+                          <div className="relative z-20 bg-transparent dark:bg-form-input">
+                            <select
+                              {...register("current_job_employment_status", { required: "Employment Status is required" })}
+                              className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-2 px-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                            >
+                              <option value="">Select Status</option>
+                              {employment_statuses.map((status, key) => (
+                                <option key={key} value={status}>
+                                  {status}
+                                </option>
+                              ))}
+                            </select>
+                            <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                              <svg
+                                className="fill-current"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <g opacity="0.8">
+                                  <path
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                                    fill=""
+                                  ></path>
+                                </g>
+                              </svg>
+                            </span>
+                          </div>
                         </td>
                         <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                           <input
