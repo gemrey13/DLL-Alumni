8fdafbe9fa42e4f8fa7d3118e08157d13adf6742
gemrey13 <gemreyranola@gmail.com>
2024-01-09 20:00:36 +0800

    fix: navabr in landing page set z in 99999

--- frontend/src/layout/LandingLayout.jsx
+++ frontend/src/layout/LandingLayout.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/shared/Footer";
 const LandingLayout = () => {
     return (
         <>
-            <div className="sticky top-0 w-full md:px-9 sm:px-0 z-[2] bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950">
+            <div className="sticky top-0 w-full md:px-9 sm:px-0 z-99999 bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950">
                 <Navbar />
             </div>
             <main>
