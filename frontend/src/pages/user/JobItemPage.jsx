b3a3af9ac2f457c03ee7a7fab19a0e6af5e770d8
gemrey13 <gemreyranola@gmail.com>
2024-01-12 09:12:06 +0800

    style: styling mobile view for job item page

--- frontend/src/pages/user/JobItemPage.jsx
+++ frontend/src/pages/user/JobItemPage.jsx
@@ -14,16 +14,16 @@ const JobItemPage = () => {
 
     return (
         <>
-            <div className="flex pt-10 pb-15">
-                <section className="flex-1 border-r-[1px] border-slate-300">
+            <div className="flex lg:flex-row flex-col pt-3 lg:pt-10 pb-15">
+                <section className="flex-1 border-r-0 lg:border-r-[1px] border-slate-300">
                     <section className="border-b-[1px] border-slate-300 p-7">
                         <h1 className="text-2xl text-black-2 font-medium py-4">
                             Mongo Django Python Homebrew Terminal Xcode with
                             some Celery. I just need a file -wtf
                         </h1>
-                        <div className="flex text-base gap-8">
+                        <div className="flex lg:flex-row flex-col text-base gap-4 lg:gap-8">
                             <p>Posted 59 minutes ago</p>
-                            <div className="flex justify-center items-center gap-2">
+                            <div className="flex lg:justify-center items-center gap-2">
                                 <HiOutlineLocationMarker />
                                 <p className="font-medium">Worldwide</p>
                             </div>
@@ -47,7 +47,7 @@ const JobItemPage = () => {
                         </p>
                     </section>
                     <section className="border-b-[1px] border-slate-300 p-7">
-                        <ul className="flex justify-between items-start">
+                        <ul className="flex flex-col lg:flex-row gap-10 lg:gap-0 justify-between items-start">
                             <li className="flex justify-center items-center gap-1 text-black-2 font-medium">
                                 <HiOutlineClock size={25} />
                                 Full-Time
@@ -84,7 +84,9 @@ const JobItemPage = () => {
                         <div className="badge badge-outline ml-0 m-4">Software development</div>
                     </section>
                 </section>
-                <aside className="py-7 pl-5 w-[20%]">
+
+
+                <aside className="py-7 pl-0 lg:pl-5 w-full lg:w-[20%]">
                     <div className="flex flex-col ">
                         <button className="btn btn-md btn-primary rounded-full mb-3">
                             Apply Now
