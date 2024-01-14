044be556e21e83d31cf7e7945ce7b6595b076bf9
gemrey13 <gemreyranola@gmail.com>
2024-01-14 08:26:26 +0800

    style: added scroll to top button in landing page

---
+++ frontend/src/components/shared/ScrollToTop.jsx
@@ -0,0 +1,40 @@
+// ScrollToTop.js
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+const ScrollToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        const scrollTop = window.scrollY;
+        setIsVisible(scrollTop > 300);
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div>
+            {isVisible && (
+                <button
+                    onClick={scrollToTop}
+                    className="fixed bottom-8 right-8 p-3 bg-black z-999999 bg-blue-500 text-white rounded-full focus:outline-none hover:bg-blue-600 transition-all duration-300">
+                    <FaArrowUp />
+                </button>
+            )}
+        </div>
+    );
+};
+
+export default ScrollToTop;
--- frontend/src/layout/LandingLayout.jsx
+++ frontend/src/layout/LandingLayout.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar";
 import Footer from "../components/shared/Footer";
+import ScrollToTop from "../components/shared/ScrollToTop";
 
 const LandingLayout = () => {
     return (
-        <>
+        <>  
+            <ScrollToTop />
             <div className="sticky top-0 w-full md:px-9 sm:px-0 z-9 bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950">
                 <Navbar />
             </div>
