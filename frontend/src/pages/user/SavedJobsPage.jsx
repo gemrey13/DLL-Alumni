901bb1c3659ce65402dcfa144f2da7b4b6eabe16
gemrey13 <gemreyranola@gmail.com>
2024-01-24 09:14:21 +0800

    ci: added title for saved jobs

--- frontend/src/pages/user/SavedJobsPage.jsx
+++ frontend/src/pages/user/SavedJobsPage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const SavedJobsPage = () => {
-  return <div>SavedJobsPage</div>;
+  return (
+    <>
+      <section className="flex flex-col flex-1 mt-0 md:mt-5 w-full mb-4">
+        <h1 className="border-b-[1px] lg:text-title-xl text-center lg:text-left text-title-md text-black-2 pb-2 lg:font-semibold font-medium">
+          Saved Jobs
+        </h1>
+      </section>
+    </>
+  );
 };
 
 export default SavedJobsPage;
