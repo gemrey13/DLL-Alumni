b41c19b56594578274415cdaae2b9977e6e849b1
gemrey13 <gemreyranola@gmail.com>
2024-01-24 20:30:56 +0800

    test: remove DOM content load in landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import Hero from "../components/landing/Hero";
 import News from "../components/landing/News";
 import Carousel from "../components/landing/Carousel";
@@ -8,23 +7,7 @@ import FAQ from "../components/landing/FAQ";
 import blob_hero from "../images/blob_hero.png";
 
 function LandingPage() {
-  const [isContentLoaded, setIsContentLoaded] = useState(false);
-
-  useEffect(() => {
-    const handleContentLoad = () => {
-      // This function will be called when the DOM content is fully loaded
-      setIsContentLoaded(true);
-    };
-
-    document.addEventListener("DOMContentLoaded", handleContentLoad);
-
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      document.removeEventListener("DOMContentLoaded", handleContentLoad);
-    };
-  }, []);
-
-  return !isContentLoaded ? (
+  return (
     <>
       <div className="w-full h-screen pt-8 md:px-16 sm:px-0 z-[-2] bg-gradient-to-r from-slate-800 via-slate-900 to-slate-950">
         <img
@@ -40,10 +23,6 @@ function LandingPage() {
       <Location />
       <FAQ />
     </>
-  ) : (
-    <div>
-      <h1>Loading...</h1>
-    </div>
   );
 }
 
