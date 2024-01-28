1c61109e32cd879d1b510a09fe82c9d1337311e8
gemrey13 <gemreyranola@gmail.com>
2024-01-28 12:00:00 +0800

    Your commit message

--- frontend/src/pages/admin/AddNews.jsx
+++ frontend/src/pages/admin/AddNews.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Breadcrumb from "../../components/admin/Breadcrumb";
 import AddJobHeader from "../../components/admin/AddJobHeader";
 import AddNewsHeader from "../../components/admin/AddNewsHeader";
+import TableNewsList from "../../components/tables/TableNewsList";
 
 const AddNews = () => {
   return (
     <>
       <Breadcrumb pageName="News " />
       <AddNewsHeader />
+      <TableNewsList />
     </>
   );
 };
