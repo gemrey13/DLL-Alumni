37862b22e7c7d70be44b95d68c8849ba02f5ca4d
gemrey13 <gemreyranola@gmail.com>
2024-01-28 09:50:04 +0800

    ci: added date and time in news modal

--- frontend/src/components/modals/NewsModal.jsx
+++ frontend/src/components/modals/NewsModal.jsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from "react";
 import { convertToTitleCase } from "../../utils/formatting";
 import baseURL from "@/apiConfig";
+import { formatDate, formatTimestamp } from "../../utils/formatting";
 
 const NewsModal = forwardRef(({ newsData, onClose }, ref) => {
   return (
@@ -9,6 +10,9 @@ const NewsModal = forwardRef(({ newsData, onClose }, ref) => {
         <h3 className="font-bold text-lg dark:text-white text-black">
           {convertToTitleCase(newsData.header)}
         </h3>
+        <span className="text-sm mr-2">{formatDate(newsData.posted_at)}</span>
+        <span className="text-sm">{formatTimestamp(newsData.posted_at)}</span>
+
         <p className="py-4">{newsData.summary}</p>
         <hr />
         <section className="sm:flex items-center gap-5">
@@ -17,7 +21,7 @@ const NewsModal = forwardRef(({ newsData, onClose }, ref) => {
             alt={newsData.header}
             className="object-scale-down h-67 lg:h-94 w-100 rounded-md"
           />
-          <p className="text-black-2">{newsData.description}</p>
+          <p className="text-black-2 dark:text-white">{newsData.description}</p>
         </section>
 
         <div className="modal-action">
