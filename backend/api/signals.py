fdb80692122b20a43c073a0e29a23558d6a31226
gemrey13 <gemreyranola@gmail.com>
2024-01-29 12:00:00 +0800

    ci: added signals for event poster image delete image if the event is deleted

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import UserProfile, News
+from .models import UserProfile, News, Event
 import os
 
 
@@ -18,4 +18,12 @@ def delete_cover_image(sender, instance, **kwargs):
             os.remove(instance.cover_image.path)
 
 
+@receiver(pre_delete, sender=Event)
+def delete_poster_image(sender, instance, **kwargs):
+    if instance.poster_image:
+        if os.path.isfile(instance.poster_image.path):
+            os.remove(instance.poster_image.path)
+
+
 pre_delete.connect(delete_cover_image, sender=News)
+pre_delete.connect(delete_poster_image, sender=Event)
