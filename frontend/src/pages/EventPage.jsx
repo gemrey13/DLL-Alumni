f5f7e6fcf7129359a75b3966c6fe0626fd0f9c78
gemrey13 <gemreyranola@gmail.com>
2024-01-30 12:00:00 +0800

    ci: added design for events

--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,14 +1,73 @@
 import React from "react";
-import News from "../components/landing/News";
-import NewsList from "../components/landing/NewsList";
+import image from "../images/news-1.png";
+import image2 from "../images/me.png";
 
 const NewsPage = () => {
   return (
     <>
-      <section className="pb-20">
-        <News />
-        <div className="lg:mt-22 mt-10">
-          <NewsList />
+      <section className="py-10 px-3 lg:py-20 lg:px-10">
+        <h2 className="text-black font-bold text-3xl mx-8">Latest Events</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
+          <div className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md">
+            <img
+              src={image}
+              alt=""
+              className="object-cover w-full h-2/3 rounded-md"
+            />
+
+            <div className="p-3">
+              <h5 className="text-lg text-black font-semibold">
+                DLLU Sports Fest 2024
+              </h5>
+              <p className="line-clamp-2 mt-4">
+                Gear up for an exciting day of sportsmanship at the DLLU Sports
+                Fest 2024. Alumni, faculty, and students are invited to
+                participate in various sports activities and enjoy a day of
+                friendly competition and camaraderie.
+              </p>
+              <p className="text-right mt-10">-DLLU Sports Committee</p>
+            </div>
+          </div>
+          <div className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md">
+            <img
+              src={image2}
+              alt=""
+              className="object-cover w-full h-2/3 rounded-md"
+            />
+
+            <div className="p-3">
+              <h5 className="text-lg text-black font-semibold">
+                DLLU Sports Fest 2024
+              </h5>
+              <p className="line-clamp-2 mt-4">
+                Gear up for an exciting day of sportsmanship at the DLLU Sports
+                Fest 2024. Alumni, faculty, and students are invited to
+                participate in various sports activities and enjoy a day of
+                friendly competition and camaraderie.
+              </p>
+              <p className="text-right mt-10">-DLLU Sports Committee</p>
+            </div>
+          </div>
+          <div className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md">
+            <img
+              src={image2}
+              alt=""
+              className="object-cover w-full h-2/3 rounded-md"
+            />
+
+            <div className="p-3">
+              <h5 className="text-lg text-black font-semibold">
+                DLLU Sports Fest 2024
+              </h5>
+              <p className="line-clamp-2 mt-4">
+                Gear up for an exciting day of sportsmanship at the DLLU Sports
+                Fest 2024. Alumni, faculty, and students are invited to
+                participate in various sports activities and enjoy a day of
+                friendly competition and camaraderie.
+              </p>
+              <p className="text-right mt-10">-DLLU Sports Committee</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
