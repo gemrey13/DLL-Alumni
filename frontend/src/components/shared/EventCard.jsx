89384bab239b3f876a6f00ee15d7a0b8927760ab
gemrey13 <gemreyranola@gmail.com>
2024-01-31 12:00:00 +0800

    build: added eventcard and dynamic data from backend

---
+++ frontend/src/components/shared/EventCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import baseURL from "@/apiConfig";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
+const EventCard = ({ eventList }) => {
+  return (
+    <>
+      {eventList &&
+        eventList.map((eventItem) => (
+          <Link
+            key={eventItem.id}
+            className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md opacity-100 transition duration-300 ease-in-out hover:opacity-70">
+            <img
+              src={`${baseURL}${eventItem.poster_image}`}
+              alt={eventItem.title}
+              className="object-cover w-full h-2/3 rounded-t-md"
+            />
+
+            <div className="p-3 h-1/3">
+              <h5 className="text-lg text-black font-semibold">
+                {eventItem.title}
+              </h5>
+              <p className="flex items-center justify-start">
+                <HiOutlineLocationMarker size={15} />
+                <span className="line-clamp-1 text-sm">
+                  {eventItem.location}
+                </span>
+              </p>
+              <p className="line-clamp-2 mt-4 text-base text-black">
+                {eventItem.description}
+              </p>
+              <p className="text-right mt-5 lg:mt-10">-{eventItem.organizer}</p>
+            </div>
+          </Link>
+        ))}
+    </>
+  );
+};
+
+export default EventCard;
--- frontend/src/pages/EventPage.jsx
+++ frontend/src/pages/EventPage.jsx
@@ -1,77 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import image from "../images/news-1.png";
 import image2 from "../images/me.png";
+import baseURL from "@/apiConfig";
+import axios from "axios";
+import toast from "react-hot-toast";
+import EventCard from "../components/shared/EventCard";
 
-const NewsPage = () => {
+const EventPage = () => {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/api/event-list/`);
+      console.log(response);
+      setData(response.data);
+    } catch (error) {
+      toast.error("Something went wrong....");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <section className="py-10 px-3 lg:py-20 lg:px-10">
         <h2 className="text-black font-bold text-3xl mx-8">Latest Events</h2>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
-          <div className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md">
-            <img
-              src={image}
-              alt=""
-              className="object-cover w-full h-2/3 rounded-md"
-            />
-
-            <div className="p-3">
-              <h5 className="text-lg text-black font-semibold">
-                DLLU Sports Fest 2024
-              </h5>
-              <p className="line-clamp-2 mt-4">
-                Gear up for an exciting day of sportsmanship at the DLLU Sports
-                Fest 2024. Alumni, faculty, and students are invited to
-                participate in various sports activities and enjoy a day of
-                friendly competition and camaraderie.
-              </p>
-              <p className="text-right mt-10">-DLLU Sports Committee</p>
-            </div>
-          </div>
-          <div className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md">
-            <img
-              src={image2}
-              alt=""
-              className="object-cover w-full h-2/3 rounded-md"
-            />
-
-            <div className="p-3">
-              <h5 className="text-lg text-black font-semibold">
-                DLLU Sports Fest 2024
-              </h5>
-              <p className="line-clamp-2 mt-4">
-                Gear up for an exciting day of sportsmanship at the DLLU Sports
-                Fest 2024. Alumni, faculty, and students are invited to
-                participate in various sports activities and enjoy a day of
-                friendly competition and camaraderie.
-              </p>
-              <p className="text-right mt-10">-DLLU Sports Committee</p>
-            </div>
-          </div>
-          <div className="m-8 flex h-150 flex-col gap-5 bg-bodydark1 rounded-md">
-            <img
-              src={image2}
-              alt=""
-              className="object-cover w-full h-2/3 rounded-md"
-            />
-
-            <div className="p-3">
-              <h5 className="text-lg text-black font-semibold">
-                DLLU Sports Fest 2024
-              </h5>
-              <p className="line-clamp-2 mt-4">
-                Gear up for an exciting day of sportsmanship at the DLLU Sports
-                Fest 2024. Alumni, faculty, and students are invited to
-                participate in various sports activities and enjoy a day of
-                friendly competition and camaraderie.
-              </p>
-              <p className="text-right mt-10">-DLLU Sports Committee</p>
-            </div>
-          </div>
+          <EventCard eventList={data} />
         </div>
       </section>
     </>
   );
 };
 
-export default NewsPage;
+export default EventPage;
