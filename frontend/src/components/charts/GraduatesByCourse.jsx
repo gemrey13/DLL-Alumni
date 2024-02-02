8266cb97422e3b283bf79ae900ce112ad2cd959a
gemrey13 <gemreyranola@gmail.com>
2024-02-02 08:03:24 +0800

    ci: added employment type chart

--- frontend/src/components/charts/GraduatesByCourse.jsx
+++ frontend/src/components/charts/GraduatesByCourse.jsx
@@ -159,7 +159,7 @@ const GraduatesByCourse = () => {
       <Loader />
     </div>
   ) : (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5">
+    <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pt-7.5 pb-5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-7">
       <div className="mb-4 justify-between gap-4 sm:flex">
         <div>
           <h3 className="text-xl font-semibold text-black dark:text-white">
--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,72 +1,87 @@
-import CardFour from '../../components/admin/CardFour.jsx';
-import CardOne from '../../components/admin/CardOne.jsx';
-import CardThree from '../../components/admin/CardThree.jsx';
-import CardTwo from '../../components/admin/CardTwo.jsx';
-import ChartOne from '../../components/admin/ChartOne.jsx';
-import ChartThree from '../../components/admin/ChartThree.jsx';
-import ChartTwo from '../../components/admin/ChartTwo.jsx';
-import ChatCard from '../../components/admin/ChatCard.jsx';
+import CardFour from "../../components/admin/CardFour.jsx";
+import CardOne from "../../components/admin/CardOne.jsx";
+import CardThree from "../../components/admin/CardThree.jsx";
+import CardTwo from "../../components/admin/CardTwo.jsx";
+import ChartOne from "../../components/admin/ChartOne.jsx";
+import ChartThree from "../../components/admin/ChartThree.jsx";
+import ChartTwo from "../../components/admin/ChartTwo.jsx";
+import ChatCard from "../../components/admin/ChatCard.jsx";
 // import MapOne from '../../../components/admin/MapOne.jsx';
-import TableOne from '../../components/admin/TableOne.jsx';
-import AlumniGraduationYearDistribution from '../../components/charts/AlumniGraduationYearDistribution.jsx';
-import EmployedWithin6Months from '../../components/charts/EmployedWithin6Months.jsx';
-import MonthlyYearDistribution from '../../components/charts/MonthlySalaryDistribution.jsx';
+import TableOne from "../../components/admin/TableOne.jsx";
+import AlumniGraduationYearDistribution from "../../components/charts/AlumniGraduationYearDistribution.jsx";
+import EmployedWithin6Months from "../../components/charts/EmployedWithin6Months.jsx";
+import MonthlyYearDistribution from "../../components/charts/MonthlySalaryDistribution.jsx";
 
 import axios from "axios";
 import React, { useState, useContext, useEffect } from "react";
 import baseURL from "@/apiConfig";
 import Loader from "../../common/Loader";
 import AuthContext from "../../context/AuthContext";
-import GenderBasedCurrentJob from '../../components/charts/GenderBasedCurrentJob.jsx';
-import TableTopPerformingCourse from '../../components/tables/TableTopPerformingCourse.jsx';
-import GraduatesByCourse from '../../components/charts/GraduatesByCourse.jsx';
-
+import GenderBasedCurrentJob from "../../components/charts/GenderBasedCurrentJob.jsx";
+import TableTopPerformingCourse from "../../components/tables/TableTopPerformingCourse.jsx";
+import GraduatesByCourse from "../../components/charts/GraduatesByCourse.jsx";
+import EmploymentType from "../../components/charts/EmploymentType.jsx";
 
 const Dashboard = () => {
   let { authTokens } = useContext(AuthContext);
   const [metrics, setMetrics] = useState({});
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState([false, '', 0]);
-
+  const [error, setError] = useState([false, "", 0]);
 
   useEffect(() => {
     fetchMetricsSum();
-  }, [])
+  }, []);
 
   const fetchMetricsSum = async () => {
     try {
       const response = await axios.get(
         `${baseURL}/api/alumni-metrics-summary`,
         {
-            headers: {
-                Authorization: `Bearer ${authTokens.access}`,
-            },
-        })
-        const newData = response.data;
-        setMetrics(newData);
-        setLoading(false);
-        // console.log('asds',newData)
+          headers: {
+            Authorization: `Bearer ${authTokens.access}`,
+          },
+        }
+      );
+      const newData = response.data;
+      setMetrics(newData);
+      setLoading(false);
+      // console.log('asds',newData)
     } catch (error) {
       setLoading(false);
       setError([true, error.code, error.response.status]);
     }
   };
 
-
-  return <>
+  return (
+    <>
       {loading ? (
         <div className="grid grid-cols-1 animate-pulse">
           <Loader />
         </div>
       ) : (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-          <CardOne data={metrics.employed_alumni} percentage={metrics.percentage_jobs} error={error}/>
-          <CardTwo data={metrics.relevant_job} percentage={metrics.percentage_relevant} error={error}/>
-          <CardThree data={metrics.promoted_alumni} percentage={metrics.percentage_promoted} error={error}/>
-          <CardFour data={metrics.alumni_profiles} percentage={metrics.percentage_alumni} error={error}/>
-          </div>
+          <CardOne
+            data={metrics.employed_alumni}
+            percentage={metrics.percentage_jobs}
+            error={error}
+          />
+          <CardTwo
+            data={metrics.relevant_job}
+            percentage={metrics.percentage_relevant}
+            error={error}
+          />
+          <CardThree
+            data={metrics.promoted_alumni}
+            percentage={metrics.percentage_promoted}
+            error={error}
+          />
+          <CardFour
+            data={metrics.alumni_profiles}
+            percentage={metrics.percentage_alumni}
+            error={error}
+          />
+        </div>
       )}
-
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         {/* <ChartOne /> */}
@@ -79,15 +94,11 @@ const Dashboard = () => {
         <MonthlyYearDistribution />
         <div className="col-span-12">
           <TableTopPerformingCourse />
-          {/* <TableOne /> */}
         </div>
-        <div className="col-span-12">
-          <GraduatesByCourse />
-          {/* <TableOne /> */}
-        </div>
-
-        {/* <ChatCard /> */}
+        <GraduatesByCourse />
+        <EmploymentType />
       </div>
-    </>;
+    </>
+  );
 };
 export default Dashboard;
